5fb36b9c9cf1f7271a7e63a40fc16dcf098e7c48
Kou <cpes9920304@gmail.com>
2022-08-30 21:36:07 +0800

    add assets file and modify template.py

--- template.py
+++ template.py
@@ -18,6 +18,8 @@ from flet import (
     IconButton,
     icons,
     Checkbox,
+    Image,
+
 )
 
 
@@ -27,7 +29,57 @@ def main(page: Page):
     page.title = "Transportaser"
     page.horizontal_alignment = "center"
     page.scroll = "adaptive"
+    page.bgcolor = "#253439"
     page.update()
+
+    Block = Text(
+        "Size 50, Normal",
+        size=50,
+        color="#253439",
+        bgcolor="#253439",
+        weight="normal",
+    )
+
+    Title = Text(
+        value="TRANPORTASER",
+        size=60,
+        color="white",
+        bgcolor="",
+        weight="bold",
+        italic=True,
+
+    )
+
+    Img = Image(
+        src=f"images/lightning_icon1.png",
+        width=100,
+        height=100,
+        fit="contain",
+    )
+
+    Body = Text(
+        value="Connect Device",
+        size=45,
+        color="white",
+        bgcolor="",
+        weight="bold",
+        italic=True,
+    )
+
+    Pair_button = Container(
+        content=Text("Pair"),
+        margin=10,
+        padding=10,
+        alignment=alignment.center,
+        bgcolor=colors.CYAN_200,
+        width=150,
+        height=150,
+        border_radius=10,
+        ink=True,
+        on_click=lambda e: print("Pairing")
+    )
+
+    page.add(Block, Title, Img, Body, Pair_button)
 
     # create application instance here
     # app = mainApp()
@@ -36,5 +88,7 @@ def main(page: Page):
     # page.add(app)
 
 
-flet.app(target=main)
-
+flet.app(
+    target=main,
+    assets_dir=f"assets"
+)
