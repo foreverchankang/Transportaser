833d8c031f7e8b58b1f496b149d7a4ca857e1257
Kou <cpes9920304@gmail.com>
2022-08-31 09:36:57 +0800

    I changed the template.py back to the origin version just like 0range said, and finished the initialUI_v1 called initialUI.py

--- template.py
+++ template.py
@@ -19,9 +19,8 @@ from flet import (
     icons,
     Checkbox,
     Image,
-
+    ElevatedButton
 )
-
 
 # Write main app code here
 # def mainApp(UserControl):
@@ -32,54 +31,7 @@ def main(page: Page):
     page.bgcolor = "#253439"
     page.update()
 
-    Block = Text(
-        "Size 50, Normal",
-        size=50,
-        color="#253439",
-        bgcolor="#253439",
-        weight="normal",
-    )
-
-    Title = Text(
-        value="TRANPORTASER",
-        size=60,
-        color="white",
-        bgcolor="",
-        weight="bold",
-        italic=True,
-
-    )
-
-    Img = Image(
-        src=f"images/lightning_icon1.png",
-        width=100,
-        height=100,
-        fit="contain",
-    )
-
-    Body = Text(
-        value="Connect Device",
-        size=45,
-        color="white",
-        bgcolor="",
-        weight="bold",
-        italic=True,
-    )
-
-    Pair_button = Container(
-        content=Text("Pair"),
-        margin=10,
-        padding=10,
-        alignment=alignment.center,
-        bgcolor=colors.CYAN_200,
-        width=150,
-        height=150,
-        border_radius=10,
-        ink=True,
-        on_click=lambda e: print("Pairing")
-    )
-
-    page.add(Block, Title, Img, Body, Pair_button)
+    page.add()
 
     # create application instance here
     # app = mainApp()
@@ -88,7 +40,4 @@ def main(page: Page):
     # page.add(app)
 
 
-flet.app(
-    target=main,
-    assets_dir=f"assets"
-)
+flet.app(target=main)
